af17c49b56fdbfa737ba744c121f774a7f99a517
JinXx ! <163841101+JinX-7@users.noreply.github.com>
2024-05-05 19:00:34 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -12,15 +12,18 @@ const {
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true })
- };
+};
+
 router.get('/', async (req, res) => {
     let num = req.query.number;
-        async function XeonPair() {
+
+    async function XeonPair() {
         const {
             state,
             saveCreds
-        } = await useMultiFileAuthState(`./session`)
-     try {
+        } = await useMultiFileAuthState(`./session`);
+        
+        try {
             let XeonBotInc = makeWASocket({
                 auth: {
                     creds: state.creds,
@@ -30,63 +33,62 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: [ "Ubuntu", "Chrome", "20.0.04" ],
              });
-             if(!XeonBotInc.authState.creds.registered) {
+
+            if(!XeonBotInc.authState.creds.registered) {
                 await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await XeonBotInc.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
+                num = num.replace(/[^0-9]/g,'');
+                const code = await XeonBotInc.requestPairingCode(num)
+                
+                if(!res.headersSent){
+                    await res.send({code});
+                }
+            }
+
             XeonBotInc.ev.on('creds.update', saveCreds)
+
             XeonBotInc.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
                 } = s;
+
                 if (connection == "open") {
-                await delay(10000);
-                    const sessionXeon = fs.readFileSync('./session/creds.json');
-                    const audioxeon = fs.readFileSync('./OneDance.mp3');
-                    XeonBotInc.groupAcceptInvite("Kjm8rnDFcpb04gQNSTbW2d");
-				const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
-				XeonBotInc.sendMessage(XeonBotInc.user.id, {
-                    audio: audioxeon,
-                    mimetype: 'audio/mp4',
-                    ptt: true
-                }, {
-                    quoted: xeonses
-                });
-				await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `*_🛑Do not share this file with anybody_*\n\n© *_Subscribe_* www.youtube.com/@s4salmanyt *_on Youtube_*` }, {quoted: xeonses});
-        await delay(100);
-        return await removeFile('./session');
-        process.exit(0)
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    XeonPair();
+                    const sessionXeon = fs.readFileSync('./session/creds.json');
+                    XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
+                    
+                    await delay(100);
+                    return await removeFile('./session');
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    XeonPair(); // إعادة المحاولة في حالة الفشل
                 }
             });
         } catch (err) {
-            console.log("service restated");
+            console.log("service restarted");
             await removeFile('./session');
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
+            
+            if(!res.headersSent){
+                await res.send({code:"Service Unavailable"});
+            }
         }
     }
+    
     return await XeonPair()
 });
 
 process.on('uncaughtException', function (err) {
-let e = String(err)
-if (e.includes("conflict")) return
-if (e.includes("Socket connection timeout")) return
-if (e.includes("not-authorized")) return
-if (e.includes("rate-overlimit")) return
-if (e.includes("Connection Closed")) return
-if (e.includes("Timed Out")) return
-if (e.includes("Value not found")) return
-console.log('Caught exception: ', err)
+    let e = String(err)
+    
+    if (e.includes("conflict")) return
+    if (e.includes("Socket connection timeout")) return
+    if (e.includes("not-authorized")) return
+    if (e.includes("rate-overlimit")) return
+    if (e.includes("Connection Closed")) return
+    if (e.includes("Timed Out")) return
+    if (e.includes("Value not found")) return
+    
+    console.log('Caught exception: ', err)
 })
 
 module.exports = router
